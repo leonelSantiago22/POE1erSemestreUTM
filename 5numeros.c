4802a1ce1990b373b0589838b96ec5a9050598ca
unknown <santiago.rosas.leonel@gmail.com>
2020-10-22 16:19:04 -0500

    5 numeros y op relacionales se compuzo la forma de como imprimir

--- 5numeros.c
+++ 5numeros.c
@@ -12,15 +12,25 @@ int main (int argc, char** argv){
     scanf("%d", &n4);
     scanf("%d", &n5);
     //comparar los numero por numero, 1 numero con los otros 4. 
-    if(n1>n2 && n1>n3 && n1>n4 && n1>n5)    printf("El numero %d es el mayor \n", n1);  //Comparamos un numero con todos los numeros 
+    if(n1>n2 && n1>n3 && n1>n4 && n1>n5)    
 
-    if(n1<n2 && n2>n3 && n2>n4 && n2>n5)    printf("El numero %d es el mayor \n", n2);  //comparamos el segundo numero con los otros 4 
+                printf("El numero %d es el mayor \n", n1);  //Comparamos un numero con todos los numeros 
 
-    if(n1<n3 && n2<n3 && n3>n4 && n3>n5)    printf("El numero %d es el mayor \n", n3);  //comparamos el tercer numero con los otros 4 
+    if(n1<n2 && n2>n3 && n2>n4 && n2>n5)   
+    
+                 printf("El numero %d es el mayor \n", n2);  //comparamos el segundo numero con los otros 4 
 
-    if(n1<n4 && n2<n4 && n3<n4 && n4>n5)    printf("El numero %d es el mayor \n", n4);  //comparamos el cuarto numero con los otros 4
+    if(n1<n3 && n2<n3 && n3>n4 && n3>n5)    
+    
+                printf("El numero %d es el mayor \n", n3);  //comparamos el tercer numero con los otros 4 
 
-    if(n1<n5 && n2<n5 && n3<n5 && n4<n5)    printf("El numero %d es el mayor \n", n5);  //comparamos el cuarto numero con los otros 4 
+    if(n1<n4 && n2<n4 && n3<n4 && n4>n5)    
+    
+                printf("El numero %d es el mayor \n", n4);  //comparamos el cuarto numero con los otros 4
+
+    if(n1<n5 && n2<n5 && n3<n5 && n4<n5)    
+    
+                printf("El numero %d es el mayor \n", n5);  //comparamos el cuarto numero con los otros 4 
     
     system("pause"); // retener la terminal 
     getchar(); //Pausar la terminal 
--- opRelacionales.c
+++ opRelacionales.c
@@ -11,17 +11,29 @@ int main (int argc, char** argv){
 
     //verificar que las relaciones se cumplan 
 
-    if (num1 > num2)    printf("El primer numero %d es mayor que el segundo numero %d \n", num1, num2); //comparamos si el num1 es mayor que el num2 
+    if (num1 > num2)    
+    
+                printf("El primer numero %d es mayor que el segundo numero %d \n", num1, num2); //comparamos si el num1 es mayor que el num2 
 
-    if (num1<num2)      printf("El primer numero %d es menor que el segundo numero %d \n", num1, num2); //comparamos si el num2 es menor que el num1 
+    if (num1<num2)      
+    
+                printf("El primer numero %d es menor que el segundo numero %d \n", num1, num2); //comparamos si el num2 es menor que el num1 
         
-    if (num1==num2)     printf("Los numero %d y %d son iguales \n", num2, num1);   // Comparamos si los numeros son iguales 
+    if (num1==num2)     
     
-    if (num1>=num2)     printf("El primer numero %d es mayor o igual a %d \n", num1, num2); //comparamos si el numero es mayor eh igual
+                printf("Los numero %d y %d son iguales \n", num2, num1);   // Comparamos si los numeros son iguales 
     
-    if (num1<=num2)     printf("El primer numero %d es menor o igual a %d \n", num1, num2); //comparamos si el numero es menor o igual
+    if (num1>=num2)    
+    
+                 printf("El primer numero %d es mayor o igual a %d \n", num1, num2); //comparamos si el numero es mayor eh igual
+    
+    if (num1<=num2)     
+    
+                printf("El primer numero %d es menor o igual a %d \n", num1, num2); //comparamos si el numero es menor o igual
 
-    if (num1!=num2)     printf("Los numeros %d y %d son diferentes",num1, num2); //comprobamos si los numeros son diferentes
+    if (num1!=num2)    
+    
+                 printf("Los numeros %d y %d son diferentes",num1, num2); //comprobamos si los numeros son diferentes
     
     
     system("pause");
