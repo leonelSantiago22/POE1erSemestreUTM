24058981e4672c0fea851ed3cbe0b7f4e244539f
unknown <santiago.rosas.leonel@gmail.com>
2020-11-09 00:19:39 -0600

    Finalizamos el parametro de modificar datos en el Eje.1 de funciones

--- Funciones/Eje1.c
+++ Funciones/Eje1.c
@@ -1,14 +1,18 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 
 
 int matricula;
 char nombre[60];
-float cal1 ,cal2, cal3, calfi;
+int cal1 ,cal2, cal3, calfi;
 int option=0;
 void menu(), agregar_alumno(), mostrar_datos(), modificar();
 int ma;
 char nombre2[60];
+
+
+
 int main ()
 {
     do{
@@ -18,8 +22,8 @@ int main ()
     switch (option)
     {
     case 1: agregar_alumno(); break;
-    case 2: modificar(); break;
-    case 3: mostrar_datos();break;
+    case 2: mostrar_datos(); break;
+    case 3: modificar();    break;
     case 4: option=4; break;
     default: printf("\nOpcion no valida\n");
     }
@@ -29,51 +33,86 @@ system("pause");
 return 0;   
 }
 
-void menu(){
+
+
+
+void menu(){//mostrar el menu 
     printf("\n************Menu de opciones*******\n");
     printf("\n1- Agregar Datos Del alumno:");
-    printf("\n2.- Modificar Datos Del alumno:");
-    printf("\n3.- Motrar Datos Del alumno:");
+    printf("\n2.- Mostrar Datos Del alumno:");
+    printf("\n3.- Modificar Datos Del alumno:");
     printf("\n4.-Salir \n");
 }
 
-void agregar_alumno(){
+
+
+void agregar_alumno(){// Agregar datos del alumno
     printf("\nIngresa la Matricula: ");
     scanf("%d", &matricula);
     printf("\nInggresa el nombre :"); 
     scanf("%s", nombre);//una cadena de caracter no lleva '&'
+    printf("\n Las calificaciones son de rango [1-100]\n");
     printf("\nIngresa La calificacion 1: ");
-    scanf("%f", &cal1);
+    scanf("%d", &cal1);
     printf("\nIngresa La calificacion 2: ");
-    scanf("%f", &cal2);
+    scanf("%d", &cal2);
     printf("\nIngresa La calificacion 3: ");
-    scanf("%f", &cal3);
+    scanf("%d", &cal3);
     printf("\nIngresa La calificacion Final: ");
-    scanf("%f", &calfi);
+    scanf("%d", &calfi);
 }
-void mostrar_datos(){
+
+
+
+void mostrar_datos(){// para mostrar los datos en la pantalla
     printf("\n--------------Datos Del alumno---------\n");
     printf("\nMatricula: %d ",matricula);
     printf("\nNombre: %s", nombre);
-    printf("\nCalificacion1: %.2f", cal1);
-    printf("\nCalificacion2: %.2f", cal2);
-    printf("\nCalificacion3: %.2f", cal3);
-    printf("\nCalificacionFinal: %.2f \n", calfi);
+    printf("\nCalificacion1: %d", cal1);
+    printf("\nCalificacion2: %d", cal2);
+    printf("\nCalificacion3: %d", cal3);
+    printf("\nCalificacionFinal: %d \n", calfi);
 }
 
+
+
 void modificar(){
-    printf("\n Ingresa '0' para no modificar");
-    printf("\nIngresa la nueva matricula: ");
-    scanf("%d", &ma);
-    if (ma==0){
-        matricula=matricula;
-    }else {   
-        matricula=ma;}
-    printf("\n Modificar tu nombre: ");
+    char ncalf[10];
+    
+    printf("\nDeseas modificar  la  matricula?[S?N] la matricula actual es %d: ", matricula);
+    scanf("%s", &ncalf);
+    if (strcmp(ncalf, "S")==0 || strcmp(ncalf, "s")==0){//Para modificar la matricula 
+        printf("\n ingrese la nueva matricula: ");
+        scanf("%d", &matricula);
+    }
+    printf("\n Modificar tu nombre:[S/N] ");
     scanf("%s", nombre2);
-    if ( nombre2=="0"){
-        nombre[60]= nombre[60];
-    }else{    
-     nombre[60] =nombre2[60];
-     }
+    if ( strcmp(nombre2, "S")==0 || strcmp(nombre2, "s")==0 ){//modificar el nombre
+        printf("\n Ingrese el nuevo nombre: ");
+        scanf("%s", nombre);
+    }
+    printf("\n Desea modificar la calificacion 1?[S/N] es %d: ", cal1);
+    scanf("%s", ncalf);
+    if (strcmp(ncalf, "S")==0 || strcmp(ncalf, "s")==0){
+        printf("\n ingrese la nueva calificacion: ");
+        scanf("%d", &cal1);
+    }
+    printf("\n Desea modificar la calificacion 2?[S/N] es  %d :", cal2);
+    scanf("%s", ncalf);
+    if (strcmp(ncalf, "S")==0 || strcmp(ncalf, "s")==0){
+        printf("\n ingrese la nueva calificacion: ");
+        scanf("%d", &cal2);
+    }
+    printf("\n Desea modificar la calificacion 3?[S/N] es %d: ", cal3);
+    scanf("%s", ncalf);
+    if (strcmp(ncalf, "S")==0 || strcmp(ncalf, "s")==0){
+        printf("\n ingrese la nueva calificacion: ");
+        scanf("%d", &cal3);
+    }
+    printf("\n Desea modificar la calificacion final?[S/N] es %d: ", calfi);
+    scanf("%s", ncalf);
+    if (strcmp(ncalf, "S")==0 || strcmp(ncalf, "s")==0){
+        printf("\n ingrese la nueva calificacion: ");
+        scanf("%d", &calfi);
+    }
 }
