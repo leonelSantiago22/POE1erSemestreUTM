552e8265e972e415ec883c8e9b165eef8049da7d
unknown <santiago.rosas.leonel@gmail.com>
2020-12-05 01:34:30 -0600

    Se hicieron cambio

---
+++ Funciones/Apuntadores.c
@@ -0,0 +1,14 @@
+//Apuntadores
+//
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+    //Son las formas de declarar un apuntador, se le define in tipo de dato.
+    int *countPtr;
+    int *prtCount;
+    int *ptr_count;
+    int *p_count;
+    int *count_p;
+    return 0;
+}
