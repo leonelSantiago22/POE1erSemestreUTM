517d8ed5232caf6c58a8a8ea3c3ae1bf24ad3a45
leonelSantiago22 <santiago.rosas.leonel@gmail.com>
2020-12-13 17:21:47 -0600

    Avances del proyecto

--- ProyectoSars/sarscov2.c
+++ ProyectoSars/sarscov2.c
@@ -2,6 +2,13 @@
 #include<stdio.h>
 
 
+#define MAX_CiUDADANOS 3
+#define MAX_NOM     60
+
+int curp[MAX_NOM]={""};
+char nombres[MAX_CiUDADANOS][MAX_NOM] = {"Leonel Santiago", "Dulce Maria", "Rosa Maria"};
+
+
 int main(){
     
 
